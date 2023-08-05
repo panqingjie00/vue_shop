d081bcc7e9d35f18233622f4e05dfb7dc854a479
潘庆杰 <958843410@qq.com>
2023-08-05 17:34:38 +0800

    add files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Button } from 'element-ui'
 
+Vue.use(Button)
 Vue.config.productionTip = false
 
 new Vue({
